8b04390861f2d2055e6614a4b0e6eb8fb1ba4f16
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-05-15 17:30:46 +0100

    configure static analysis tools and correct errors from SCA

--- app/src/main/java/idrisadetunmbi/githubusers/presenters/GithubUsersPresenter.java
+++ app/src/main/java/idrisadetunmbi/githubusers/presenters/GithubUsersPresenter.java
@@ -1,13 +1,14 @@
 package idrisadetunmbi.githubusers.presenters;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import java.util.List;
 
-import idrisadetunmbi.githubusers.views.GithubUserView;
 import idrisadetunmbi.githubusers.models.GithubUser;
 import idrisadetunmbi.githubusers.models.GithubUsersResponse;
 import idrisadetunmbi.githubusers.services.GithubService;
+import idrisadetunmbi.githubusers.views.GithubUserView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,24 +29,25 @@ public class GithubUsersPresenter {
                 .getAPI()
                 .getUsers()
                 .enqueue(new Callback<GithubUsersResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<GithubUsersResponse> call,
-                                   @NonNull Response<GithubUsersResponse> response) {
-                GithubUsersResponse data = response.body();
-                if (data != null && data.getGithubUsers() != null ) {
-                    List<GithubUser> githubUsers = data.getGithubUsers();
-                    mGithubUserView.githubUsersReady(githubUsers);
-                }
-            }
+                    @Override
+                    public void onResponse(@NonNull Call<GithubUsersResponse> call,
+                                           @NonNull Response<GithubUsersResponse> response) {
+                        GithubUsersResponse data = response.body();
+                        if (data != null && data.getGithubUsers() != null) {
+                            List<GithubUser> githubUsers = data.getGithubUsers();
+                            mGithubUserView.githubUsersReady(githubUsers);
+                        }
+                    }
 
-            @Override
-            public void onFailure(@NonNull Call<GithubUsersResponse> call, @NonNull Throwable t) {
-                try {
-                    throw new InterruptedException("Something went wrong");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+                    @Override
+                    public void onFailure(@NonNull Call<GithubUsersResponse> call,
+                                          @NonNull Throwable t) {
+                        try {
+                            throw new InterruptedException("Something went wrong");
+                        } catch (InterruptedException e) {
+                            Log.d("ERROR_RETRIEVING_USERS", e.getLocalizedMessage());
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/idrisadetunmbi/githubusers/views/UsersListActivity.java
+++ app/src/main/java/idrisadetunmbi/githubusers/views/UsersListActivity.java
@@ -1,7 +1,7 @@
 package idrisadetunmbi.githubusers.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
